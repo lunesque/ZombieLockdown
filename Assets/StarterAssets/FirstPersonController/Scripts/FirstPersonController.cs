ce6a7d8ded28617a0c08d16708c5edf11361f5c3
Hang Nguyen <157104064+lunesque@users.noreply.github.com>
2026-03-27 14:23:30 +0100

    added OSC Jack + fixed bugs

--- Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -52,7 +52,7 @@ namespace StarterAssets
 		public float BottomClamp = -90.0f;
 
 		// cinemachine
-		private float _cinemachineTargetPitch;
+		private float _cinemachineTargetPitch = 25.0f;
 
 		// player
 		private float _speed;
@@ -137,7 +137,9 @@ namespace StarterAssets
 				//Don't multiply mouse input by Time.deltaTime
 				float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
 				
+				/*
 				_cinemachineTargetPitch += _input.look.y * RotationSpeed * deltaTimeMultiplier;
+				*/
 				_rotationVelocity = _input.look.x * RotationSpeed * deltaTimeMultiplier;
 
 				// clamp our pitch rotation
